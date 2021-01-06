eb4d63165334c9460c5cf9c412478c493fbbff03
rbong <r.l.bongers@gmail.com>
2021-01-05 23:25:02 -0500

    Fix quote logic

--- mgbdis.py
+++ mgbdis.py
@@ -687,9 +687,9 @@ class Bank:
 
             byte = rom.data[address]
             if byte >= 0x20 and byte < 0x7F:
+                if chr(byte) == '"':
+                    text += '\\'
                 text += chr(byte)
-            elif byte == ord('"'):
-                text += '\\"'
             else:
                 if len(text):
                     values.append('"{}"'.format(text))
